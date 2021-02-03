30f4e1984b82bc99cefc4f027287b778634b409c
dani <dani.zaghian@gmail.com>
2021-02-03 12:17:04 -0800

    adds ruby/js solutions an async

--- practice.js
+++ practice.js
@@ -139,3 +139,47 @@ function sumStuff(nums1, nums2) {
 }
 
 // sumStuff([1, 5, 10], [100, 500, 1000]);
+
+
+// Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is ["a", "b", "c", "d", "e", "f"], the output should be ["a", "c", "e"].
+
+function selectEvenItems(strings) {
+   var result = [];
+   var index = 0;
+   strings.forEach(function(string) {
+     if (index % 2 === 0) {
+       result.push(string);
+     }
+     index++;
+   });
+   return result;
+}
+
+// console.log(selectEvenItems(["a", "b", "c", "d", "e", "f"]));
+
+// Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
+
+function max(numbers) {
+  var currentMax = numbers[0];
+  numbers.forEach(function(number) {
+    if (number > currentMax) {
+      currentMax = number;
+    }
+  });
+  return currentMax;
+}
+
+// console.log(max([5, 4, 8, 1, 2]));
+
+// Write a method that accepts one argument - an array of numbers that are in ascending order. The method that returns a new array with the same values in descending order. However, do not use the "reverse" method built into Ruby.
+function descending(numbers) {
+  var result = [];
+  var index = numbers.length - 1;
+  for (var i = 0; i < numbers.length; i++) {
+    result.push(numbers[index]);
+    index--;
+  }
+  return result;
+}
+
+// console.log(descending([1, 3, 5, 7]));
--- practice.rb
+++ practice.rb
@@ -140,3 +140,44 @@ def sum_stuff(nums1, nums2)
 end
 
 # sum_stuff([1, 5, 10], [100, 500, 1000])
+
+
+# Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is ["a", "b", "c", "d", "e", "f"], the output should be ["a", "c", "e"].
+
+def select_even_items(strings)
+  result = []
+  index = 0
+  strings.each do |string|
+    if index % 2 == 0
+      result << string
+    end
+    index = index + 1
+  end
+  result
+end
+# p select_even_items(["a", "b", "c", "d", "e", "f"])
+
+# Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
+
+def max(numbers)
+  current_max = numbers[0]
+  numbers.each do |number|
+    if number > current_max
+      current_max = number
+    end
+  end
+  current_max
+end
+# p max([5, 4, 8, 1, 2])
+
+# Write a method that accepts one argument - an array of numbers that are in ascending order. The method that returns a new array with the same values in descending order. However, do not use the "reverse" method built into Ruby.
+def descending(numbers)
+  result = []
+  index = numbers.length - 1
+  numbers.length.times do
+    result << numbers[index]
+    index = index - 1
+  end
+  result
+end
+# p descending([1, 3, 5, 7])
