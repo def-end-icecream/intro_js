7523dd61ce76be01dc1b0f0a9587435536b07284
dani <dani.zaghian@gmail.com>
2021-02-03 11:14:35 -0800

    adds ruby/js solutions

--- practice.js
+++ practice.js
@@ -45,31 +45,97 @@ cityPopulations.sf = 800000;  // same as cityPopulations["sf"]
 
 
 // Write a method that prints out every number from 1 to 100. 
+function oneToAHundred() {
+  var number = 0;
+  while (number < 100) {
+    number++;
+    console.log(number);
+  }
+}
+// oneToAHundred();
 
 
 // Write a method that prints out every other number from 1 to 100. (That is, 1, 3, 5, 7 … 99).
+function oneToAHundredOdd() {
+  var number = 0;
+  while (number < 100) {
+    if (number % 2 === 1) {
+      console.log(number);
+    }
+    number++;
+  }
+}
+// oneToAHundredOdd();
 
 
 // Write a method that accepts an array of numbers as a parameter, and counts how many 55’s there are in the array.
+function numberOf55s(numbers) {
+  var count = 0;
+  numbers.forEach(function(number) {
+    if (number === 55) {
+      count++;
+    }
+  });
+  console.log(count);
+}
+// numberOf55s([1, 55, 23, 55, 31, 2, 55, 1002, 55]);
 
 
 // Write a method that accepts an array of strings and returns a new array that has the string "awesomesauce" inserted between every string. 
 // For example, if the initial array is ["a", "b", "c", "d", "e"], then the returned array should be ["a", "awesomesauce", "b", "awesomesauce", "c", "awesomesauce", "d", "awesomesauce", "e"].
+function awesomesauced(strings) {
+  var index = 0;
+  var awesomesaucedArray = [];
+
+  while (index < strings.length) {
+    awesomesaucedArray.push(strings[index]);
+    if (index != strings.length - 1) {
+      awesomesaucedArray.push("awesomesauce");
+    }
+    index++;
+  }
+  return awesomesaucedArray;
+}
+// console.log(awesomesauced(["a", "b", "c", "d", "e"]));
 
 
 // Start with the hash: item_amounts = {chair: 5, table: 2}
 // Someone just bought two chairs. Change the hash such that the chair amount is 3.
 // The final result should be: {chair: 3, table: 2}
+var itemAmounts = {chair: 5, table: 2};
+itemAmounts.chair -= 2;
+// console.log(itemAmounts);
 
 
 // Start with the hash: item_amounts = {chair: 5, table: 2}
 // You received 7 desks to sell. Change the hash to include desks.
 // The final result should be: {chair: 5, table: 2, desk: 7}
+var itemAmounts = {chair: 5, table: 2}
+itemAmounts.desks = 7;
+// console.log(itemAmounts);
 
 
 // Write a method that accepts a number and returns its factorial.
 // For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+function factorial(number) {
+  var product = 1;
+  while (number > 1) {
+    product *= number;
+    number--;
+  }
+  return product;
+}
+// console.log(factorial(5));
 
 
 // Write a method that accepts two arrays of numbers, and prints the sum of every combination of numbers from first and second array. 
 // For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should print a list: 101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+function sumStuff(nums1, nums2) {
+  nums1.forEach(function(num1) {
+    nums2.forEach(function(num2) {
+      console.log(num1 + num2);
+    });
+  });
+}
+
+// sumStuff([1, 5, 10], [100, 500, 1000]);
